2c31b7d1b62ad9dad4153507149714026a1db6f9
Jtolen543 <Jtolen543@gmail.com>
2025-11-21 17:31:40 -0500

    remove old

--- webapp/src/db/index.ts
+++
@@ -1,6 +0,0 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { schemaTables } from "./schemas";
-
-export const db = drizzle(process.env.DATABASE_URL as string, {
-    schema: schemaTables
-})
--- webapp/src/hooks/use-debounce.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-export function useDebounce(value: any, callback: () => void, interval: number = 30000) {
-    useEffect(() => {
-        const id = setTimeout(callback, interval)
-        return () => clearTimeout(id)
-    }, [value, callback, interval])
-}
